a8cd57393a89f99fa1aff944cdab9f35ba96fbd4
Tom Craig <thetomcraig@icloud.com>
2024-06-11 11:20:04 -0700

    don't save, rerun it, pager, dripper/stacker/dub space

--- user.py
+++ user.py
@@ -1,7 +1,28 @@
-from talon import actions, app
+from talon import Context, Module, actions, app
+
+ctx = Context()
+mod = Module()
+
+@ctx.action_class("user")
+class UserActions:
+    def select_dont_save():
+        actions.key("space")
 
 
 def disable():
     actions.speech.disable()
 
 app.register("ready", disable)
+
+@mod.action_class
+class Actions:
+    def select_dont_save():
+        """Select "don't save" from the dialog"""
+
+#     def inside_trip_graves():
+#         actions.insert('```')
+#         actions.insert('\n')
+#         actions.insert('```')
+#         actions.sleep("500ms")
+#         actions.key("up")
+#         actions.insert('\n')
--- user.talon
+++ user.talon
@@ -17,9 +17,22 @@ slapper:
     edit.line_insert_down()
     edit.line_insert_down()
 
+dub space: insert('  ')
 trip quote: insert('"""')
 trip grave: insert('```')
 cent finish: insert('.  ')
+dripper: insert(', ')
+stacker: insert(': ')
 splay <number>: user.move_window_to_screen(number)
 
+rerun it: 
+    user.switcher_focus("term")
+    key(up)
+    key(enter)
+    sleep(200ms)
+    user.switcher_focus_last()
+    
+pager: edit.page_down()
+
+don't save: user.select_dont_save()
 # inside trip graves: user.inside_trip_graves()
