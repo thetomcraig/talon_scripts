0fdf1488ebc198000a1cb793a23262b7ddae3406
Tom Craig <thetomcraig@icloud.com>
2024-06-27 11:19:15 -0700

    window management, show preferences, manage snippets and secrets

--- user.py
+++ user.py
@@ -1,4 +1,7 @@
+import os
+
 from talon import Context, Module, actions, app
+from talon.mac import applescript
 
 ctx = Context()
 mod = Module()
@@ -8,6 +11,24 @@ class UserActions:
     def select_dont_save():
         actions.key("space")
 
+    def full_screen_window():
+        actions.key("cmd-ctrl-f")
+
+    def show_preferences():
+        actions.key("cmd-,")
+
+    def enter_secret(desired_secret: str):
+        """Read from secret text file and enter text"""
+        secret_dict = {}
+        user_dir_path = os.path.join(os.path.expanduser('~'), '.talon', 'user', 'talon_scripts')
+
+        with open(os.path.join(user_dir_path, 'secrets.txt'), 'r') as f:
+            for line in f.readlines():
+                key, value = line.strip().split(',')
+                secret_dict[key] = value
+
+        actions.insert(secret_dict[desired_secret])
+        actions.key("enter")
 
 def disable():
     actions.speech.disable()
@@ -16,9 +37,19 @@ app.register("ready", disable)
 
 @mod.action_class
 class Actions:
+
     def select_dont_save():
         """Select "don't save" from the dialog"""
 
+    def full_screen_window():
+        """Make the window full screen"""
+
+    def show_preferences():
+        """Open the preferences"""
+
+    def enter_secret(desired_secret: str): 
+        """Enter the secret"""
+        
 #     def inside_trip_graves():
 #         actions.insert('```')
 #         actions.insert('\n')
@@ -26,3 +57,4 @@ class Actions:
 #         actions.sleep("500ms")
 #         actions.key("up")
 #         actions.insert('\n')
+
